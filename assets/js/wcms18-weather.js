f9becb7ced631c66e9ba328225966681bb1e8041
Johan Nordström <johan@digitalvillage.se>
2019-05-28 15:47:54 +0200

    Yay AJAX!

--- assets/js/wcms18-weather.js
+++ assets/js/wcms18-weather.js
@@ -2,6 +2,17 @@
 
 	$(document).ready(function(){
 		// fire some async request
+		console.log("Firing away request");
+
+		$.post(
+			my_ajax_obj.ajax_url, // URL
+			{
+				action: 'get_current_weather'
+			}, // Data to send to server
+			function(response) {
+				console.log("GOT RESPONSE!!!! YAY!!", response);
+			}
+		);
 
 		// deal with response 😎
 	});
--- wcms18-weather.php
+++ wcms18-weather.php
@@ -24,5 +24,19 @@ function w18ww_enqueue_styles() {
 	wp_enqueue_style('wcms18-weather', plugin_dir_url(__FILE__) . 'assets/css/wcms18-weather.css');
 
 	wp_enqueue_script('wcms18-weather', plugin_dir_url(__FILE__) . 'assets/js/wcms18-weather.js', ['jquery'], false, true);
+	wp_localize_script('wcms18-weather', 'my_ajax_obj', [
+		'ajax_url' => admin_url('admin-ajax.php'),
+	]);
 }
 add_action('wp_enqueue_scripts', 'w18ww_enqueue_styles');
+
+/**
+ * Respond to AJAX request for 'get_current_weather'
+ */
+function w18ww_ajax_get_current_weather() {
+	$current_weather = owm_get_current_weather('Lund', 'SE');
+
+	wp_send_json($current_weather);
+}
+add_action('wp_ajax_get_current_weather', 'w18ww_ajax_get_current_weather');
+add_action('wp_ajax_nopriv_get_current_weather', 'w18ww_ajax_get_current_weather');
