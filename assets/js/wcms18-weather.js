f80666e460589bf8f66310f8ba89a5eef2b2992e
Johan Nordström <johan@digitalvillage.se>
2019-05-29 12:29:41 +0200

    Moved retrieving of current weather into it's own function that can be called individually from each widget-instance.

--- assets/js/wcms18-weather.js
+++ assets/js/wcms18-weather.js
@@ -1,20 +1,28 @@
 (function($){
 
 	$(document).ready(function(){
-		// fire some async request
-		console.log("Firing away request");
-
-		$.post(
-			my_ajax_obj.ajax_url, // URL
-			{
-				action: 'get_current_weather'
-			}, // Data to send to server
-			function(response) {
-				console.log("GOT RESPONSE!!!! YAY!!", response);
-			}
-		);
-
-		// deal with response 😎
 	});
 
 })(jQuery);
+
+function w18ww_get_current_weather(widget_id) {
+	// fire some async request
+	console.log("Firing away request for widget " + widget_id);
+
+	var url = wcms18_weather_settings.ajax_url,
+		payload = {
+			action: 'get_current_weather',
+		};
+
+	jQuery.post(
+		url, // URL
+		payload, // Data to send to server
+		function(data) {
+			console.log("GOT RESPONSE for widget " + widget_id + "!!!! YAY!!", data);
+			var output = "";
+			output += '<strong>Temperature:</strong> ' + data.temperature + '&deg; C<br>';
+			output += '<strong>Humidity:</strong> ' + data.humidity + '%<br>';
+			jQuery('#' + widget_id + ' .current-weather').html(output);
+		}
+	);
+}
