9966115053ea31a709e1ac90dc02efd4207281df
Johan Nordström <johan@digitalvillage.se>
2019-05-29 12:24:20 +0200

    Renamed exposed JavaScript-variable

--- wcms18-weather.php
+++ wcms18-weather.php
@@ -23,8 +23,8 @@ add_action('widgets_init', 'w18ww_widgets_init');
 function w18ww_enqueue_styles() {
 	wp_enqueue_style('wcms18-weather', plugin_dir_url(__FILE__) . 'assets/css/wcms18-weather.css');
 
-	wp_enqueue_script('wcms18-weather', plugin_dir_url(__FILE__) . 'assets/js/wcms18-weather.js', ['jquery'], false, true);
-	wp_localize_script('wcms18-weather', 'my_ajax_obj', [
+	wp_enqueue_script('wcms18-weather-js', plugin_dir_url(__FILE__) . 'assets/js/wcms18-weather.js', ['jquery'], false, true);
+	wp_localize_script('wcms18-weather-js', 'wcms18_weather_settings', [
 		'ajax_url' => admin_url('admin-ajax.php'),
 	]);
 }
